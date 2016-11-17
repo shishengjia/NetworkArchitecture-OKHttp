144189cb886188201b635600aa26ddda31926ba3
shishengjia <shishengjia1@live.com>
2016-11-17 23:30:25 +0800

    测试同步请求和异步请求

--- lib/src/main/java/com/shishengjia/http/lib/testOKHttp.java
+++ lib/src/main/java/com/shishengjia/http/lib/testOKHttp.java
@@ -2,6 +2,8 @@ package com.shishengjia.http.lib;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -12,20 +14,46 @@ import okhttp3.Response;
 
 public class testOKHttp {
 
-    public static void main(String[] args) {
-        String url = "https://www.baidu.com";
+    //同步请求
+    public static void sendSyncRequest(String url) {
+        //请求前获取当前线程ID
+        System.out.println(Thread.currentThread().getId());
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
-
         try {
             Response response = client.newCall(request).execute();
-            if(response.isSuccessful()){
-                System.out.println(response.body().string());
+            if (response.isSuccessful()) {
+                //请求后获取当前线程ID
+                System.out.print(Thread.currentThread().getId());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    //异步请求
+    public static void sendAsyncRequest(String url) {
+        //请求前获取当前线程ID
+        System.out.println(Thread.currentThread().getId());
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println("Error");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //请求后获取当前线程ID
+                System.out.println(Thread.currentThread().getId());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+//        sendSyncRequest("https://www.baidu.com");
+        sendAsyncRequest("https://www.baidu.com");
 
     }
 }
